d2b0601f7f7f527340642e6bbb092eddd8d0198d
Jesus1994-7 <jesus1994_cg@hotmail.com>
2020-08-24 12:26:23 +0200

    Create jwt, token

--- app.js
+++ app.js
@@ -5,9 +5,16 @@ var bodyParser = require('body-parser');
 
 var app = express();
 
+//carga de rutas
+var user_routes = require('./routes/user');
+
+
 //middleware
     //cada peticion la vamos a convertir a json
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
+
+//rutas
+app.use('/api', user_routes);
 
 module.exports = app;
