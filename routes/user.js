cc47f957b8e5bae8ec3dbf9315be3fdca9a8b46b
Jesus1994-7 <jesus1994_cg@hotmail.com>
2020-08-24 13:06:00 +0200

    Add auth at routes for reclame token at some routes

--- routes/user.js
+++ routes/user.js
@@ -1,12 +1,13 @@
 'use strict'
 
-var express = require('express');
-var UserController = require('../controllers/user');
+const express = require('express');
+const UserController = require('../controllers/user');
 
-var api = express.Router();
+const api = express.Router();
+const md_auth = require('../middlewares/auth');
 
 api.get('/home', UserController.home);
-api.get('/pruebas', UserController.pruebas);
+api.get('/pruebas', md_auth.ensureAuth , UserController.pruebas);
 api.post('/register', UserController.saveUser);
 api.post('/login', UserController.login);
 
