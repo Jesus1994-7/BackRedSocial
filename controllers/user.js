b6d3fef5103e3f29ca353fe66336d92b9c761cce
Jesus1994-7 <jesus1994_cg@hotmail.com>
2020-08-25 11:29:15 +0200

    Add method for upload images

--- controllers/user.js
+++ controllers/user.js
@@ -4,6 +4,8 @@ const User = require('../models/user');
 const bcrypt = require('bcrypt-nodejs');
 const jwt = require('../services/jwt');
 const mongoosePaginate = require('mongoose-pagination');
+const fs = require('fs'); //trabajar con archivos
+const path = require('path'); //trabajar con rutas
 const { use } = require('../routes/user');
 
 const UserController = {
@@ -104,64 +106,114 @@ const UserController = {
         });
     },
 
-//datos de usuario
+    //datos de usuario
 
-    async getUser(req,res){
+    async getUser(req, res) {
         const userId = req.params.id;
 
         User.findById(userId, (err, user) => {
-            if(err) return res.status(500).send({message: 'Error en la petición'});
+            if (err) return res.status(500).send({ message: 'Error en la petición' });
 
-            if(!user) return res.status(404).send({message: 'El usuario no existe'});
+            if (!user) return res.status(404).send({ message: 'El usuario no existe' });
 
-            return res.status(200).send({user});
+            return res.status(200).send({ user });
         });
     },
 
-//devolver listado de usuarios paginados
+    //devolver listado de usuarios paginados
 
-    async getUsers(req,res) {
+    async getUsers(req, res) {
         //recogemos el id del usuario logueado en este momento
         const identity_user_id = req.user.sub;
 
         let page = 1;
-        if(req.params.page){
-            page = req.params.page; 
+        if (req.params.page) {
+            page = req.params.page;
         }
         //cantidad de usuarios logueados por pagina
         var itemsPerPage = 5;
-        
-        User.find().sort('_id').paginate(page, itemsPerPage, (err, users, total) => {
-            if(err) return res.status(500).send({message: 'Error en la peticion'});
 
-            if(!users) return res.status(404).send({message: 'No hay usuarios disponibles'});
+        User.find().sort('_id').paginate(page, itemsPerPage, (err, users, total) => {
+            if (err) return res.status(500).send({ message: 'Error en la peticion' });
+
+            if (!users) return res.status(404).send({ message: 'No hay usuarios disponibles' });
 
             return res.status(200).send({
                 users,
                 total,
-                pages: Math.ceil(total/itemsPerPage) //saca el numero de paginas que van a existir
+                pages: Math.ceil(total / itemsPerPage) //saca el numero de paginas que van a existir
             });
         });
     },
 
-    async updateUser(req,res) {
+    async updateUser(req, res) {
         const userId = req.params.id;
         const update = req.body;
 
         //hay que borrar la password y actualizarla por separado
         delete update.password;
 
-        if(userId != req.user.sub){
-            return res.status(500).send({message: 'No tienes permiso para actualizar los datos del usuario'})
+        if (userId != req.user.sub) {
+            return res.status(500).send({ message: 'No tienes permiso para actualizar los datos del usuario' })
         }
         //busco usuario por id, le paso los datos a actualizar y el objeto modificado(new)
-        User.findByIdAndUpdate(userId, update, {new:true}, (err, userUpdated) => {
-            if(err) return res.status(500).send({message: 'Error en la peticion'});
+        User.findByIdAndUpdate(userId, update, { new: true }, (err, userUpdated) => {
+            if (err) return res.status(500).send({ message: 'Error en la peticion' });
 
-            if(!userUpdated) return res.status(404).send({message: 'No se ha podido actualizar el usuario'});
+            if (!userUpdated) return res.status(404).send({ message: 'No se ha podido actualizar el usuario' });
 
-            return res.status(200).send({user: userUpdated})
+            return res.status(200).send({ user: userUpdated })
         });
+    },
+
+    //imagen/avatar del usuario
+
+    async uploadImage(req, res) {
+        const userId = req.params.id;
+
+        if (req.files) {
+            const file_path = req.files.image.path //aqui sacamos el path de la imagen que queremos subir
+            console.log(file_path);
+
+            const file_split = file_path.split('\\'); //separamos los segmentos de la path en un array
+            console.log(file_split);
+
+            var file_name = file_split[2]; //seleccionamos la posicion 2 del array(nombre del archivo)
+            console.log(file_name);
+
+            const ext_split = file_name.split('\.'); //sacamos la extension del archivo para ver si es una imagen el archivo a subir
+            console.log(ext_split);
+
+            const file_ext = ext_split[1]; //seleccionamos la extension del fichero
+            console.log(file_ext);
+
+            //validacion de id para dejar cambiar la img
+            if (userId != req.user.sub) {
+               return removeFilesOfUploads(res, file_path, 'No tienes permisos para actualizar los datos del usuario' );
+            }
+
+            if (file_ext == 'png' || file_ext == 'jpg' || file_ext == 'jpeg' || file_ext == 'gif') {
+                // Actualizar el usuario con la imagen
+                User.findByIdAndUpdate(userId, {image: file_name}, {new:true}, (err, userUpdated) => {
+                    if (err) return res.status(500).send({ message: 'Error en la peticion' });
+
+                    if (!userUpdated) return res.status(404).send({ message: 'No se ha podido actualizar el usuario' });
+        
+                    return res.status(200).send({ user: userUpdated })
+                });
+            } else {
+                return removeFilesOfUploads(res, file_path, 'Extension no valida' );
+            }
+
+        } else {
+            return res.status(200).send({ message: 'No se han subido imagenes' });
+        }
     }
+}
+//funcion para remover imagenes no validas
+function removeFilesOfUploads(res, file_path, message) {
+    fs.unlink(file_path, (err) => {
+        return res.status(200).send({ message: message });
+    });
 }
 module.exports = UserController;
